602c2eff30304d3ab9a31788a07995121c07c25e
Dave McCollough <davemccollough@gmail.com>
2020-12-28 22:09:15 -0600

    Python day 1

---
+++ band_name_generator/app.py
@@ -0,0 +1,12 @@
+#Greeting
+print("Welcome to the Band Name Generator!  We'll help you name your new band!")
+
+#Get city name
+city_name = input("What city did you grow up in?\n")
+
+#Get pet name
+pet_name = input("What was the name of your first pet?\n")
+
+#Print band name
+print(f"The name of your band is the {city_name} {pet_name} Rockers!!")
+
